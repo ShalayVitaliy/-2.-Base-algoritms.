14b7e44af072113bd9cc990927edd7cf9d112579
Vitaliy <vitalik.shalay2002@yandex.ru>
2023-03-14 11:22:59 +0300

    Completed Exercise002

---
+++ Exercise002_ThirdNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a <= 99 || a >= 1000)
+{
+    Console.Write("Вы велли не трёхзначное число!");
+}
+else
+    Console.Write(a % 10);
